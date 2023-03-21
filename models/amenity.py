85cab1618019ea3426b5c2dc03543323356d947a
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-03-21 12:44:48 -0700

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
+from sqlalchemy.orm import relationship, backref
 
 
 class Amenity(BaseModel):
-    name = ""
+    """
+    class for Amennity,
+    inherits from BaseModel and Base
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place ", secondary="place_amenity",
+                                   back_populates="amenities")
