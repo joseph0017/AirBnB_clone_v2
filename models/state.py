b94c19b9d53b939d28b72cd4484aee81ad669fea
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-03-21 03:39:07 -0700

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, Integer, Table, ForeignKey, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from models import storage
+from models.city import City
+
+Base = declarative_base()
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",
+                           cascade="all, delete-orphan")
+    
+    @property
+    def cities(self):
+        """returns list of city instances"""
+        get_cities = storgae.all(City).items()
+        for key, value in get_cities:
+            if value.state_id == self.id:
+                return value
