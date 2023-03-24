f0400a5ea41de7faa35777e23603335667feb590
joseph0017 <josephogiku96@gmail.com>
2023-03-23 23:37:11 -0700

    Revert "Update amenity.py"
    This reverts commit 85cab1618019ea3426b5c2dc03543323356d947a.
    
    previous work

--- models/amenity.py
+++ models/amenity.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
-from sqlalchemy.orm import relationship, backref
 
 
 class Amenity(BaseModel):
-    """
-    class for Amennity,
-    inherits from BaseModel and Base
-    """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place ", secondary="place_amenity",
-                                   back_populates="amenities")
+    name = ""
