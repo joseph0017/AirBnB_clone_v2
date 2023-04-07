c29b1b6a057fa0a956d56ec6bb585c489f09d806
Usman Sharafa <usmansharafa04@gmail.com>
2023-04-07 02:18:59 +0000

    Task 2 solved

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+from datetime import datetime
+from fabric.api import env, local, run, put
+
+env.hosts = ['100.26.17.152', '54.89.109.11']
+env.user = 'ubuntu'
+env.key_filename = '~/id_rsa'
+
+
+def do_deploy(archive_path):
+    """
+    Distributes an archive to web servers
+    """
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        archive_name = os.path.basename(archive_path)
+        put(archive_path, "/tmp/{}".format(archive_name))
+        run("mkdir -p /data/web_static/releases/{}/".format(
+            archive_name[:-4]))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
+            archive_name, archive_name[:-4]))
+        run("rm /tmp/{}".format(archive_name))
+        run("mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".format(
+                archive_name[:-4], archive_name[:-4]))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(
+            archive_name[:-4]))
+        run("rm -rf /data/web_static/current")
+        ln_dir = "/data/web_static/current"
+        run("ln -s /data/web_static/releases/{}/ {}".format(
+            ln_dir, archive_name[:-4]))
+        return True
+    except:
+        return False
