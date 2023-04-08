0f31a3efed2356ca7b0ebe969bc0877544b5e25a
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-04-08 08:16:32 -0700

    modified fabric

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,8 +11,8 @@ def do_pack():
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
             local("mkdir versions")
-        file_name = f"versions/web_static_{date}.tgz"
-        local(f"tar -cvzf {file_name} web_static")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
         return file_name
     except:
         return None
