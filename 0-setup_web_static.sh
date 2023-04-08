cb90ebbbe6c458a7ac3a934ef94726b45d5be928
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-04-08 02:29:29 -0700

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-# This script sets up web servers for deployment of web_static
-apt-get install -y nginx
-mkdir -p /data/web_static/{releases,shared}
-mkdir /data/web_static/releases/test/
-echo -e "<html>\n  <head></head>\n  <body>This is a simple content.</body>\n</html>" > /data/web_static/releases/test/index.html
+# Bash script that sets up your web servers for the deployment of web_static
+sudo apt-get install -y nginx
+sudo mkdir -p /data/web_static/{releases,shared}
+sudo mkdir /data/web_static/releases/test/
+echo -e "<html>\n  <head></head>\n  <body>Just Testing Alx content.</body>\n</html>" > /data/web_static/releases/test/index.html
 rm -f /data/web_static/current
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 CONFIG="\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tindex index.html;\n\t}"
 sed -i "40i\ $CONFIG" /etc/nginx/sites-available/default
-service nginx restart
+sudo service nginx restart
