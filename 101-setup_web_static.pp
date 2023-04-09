8f94642558717d2bbb39e75280ce12c8de2ee054
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-04-09 06:29:37 -0700

    Update 101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,5 +1,22 @@
 # Puppet script that sets up your web servers for the deployment of web_static
 
+# Nginx configuration => /etc/nginx/sites-available/default
+$nginx_configuration = "server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        #The header
+        add_header X-Served-By $HOSTNAME;
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+        server_name _;
+	
+	error_page 404 /page_not_found.html;
+        location /404 {
+                root /var/www/html;
+		internal;
+        }
+}"
+
 package { 'nginx':
   ensure   => 'present',
   provider => 'apt'
@@ -46,6 +63,11 @@ file { '/var/www':
 file { '/var/www/html':
   ensure => 'directory'
 }
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_configuration
+} 
 
 file { '/var/www/html/index.html':
   ensure  => 'present',
