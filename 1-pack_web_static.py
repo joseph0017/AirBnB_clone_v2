45a4b1d69669329abed4f168914c174a57145744
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-04-09 02:58:58 -0700

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,8 +7,12 @@ from datetime import datetime
 
 def do_pack():
     """Creates a tgz archive from the contents of the web_static folder"""
-    local("mkdir -p versions")
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_name = "versions/web_static_{}.tgz".format(timestamp)
-    local("tar -cvzf {} web_static".format(file_name))
-    return file_name
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
