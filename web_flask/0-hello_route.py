233329b4374b32eeacbd120bc2dea6f1d9e1944e
joseph0017 <josephogiku96@gmail.com>
2023-04-20 08:56:32 -0700

    flask is cool

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,15 @@
-from . import create_app
+#!/usr/bin/python3
+"""
+a script that starts a Flask web application
+"""
+from flask import Flask
 
-app = create_app()
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def home():
+    """sends a GET request to the homepage"""
+    return ("Hello HBNB!")
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
