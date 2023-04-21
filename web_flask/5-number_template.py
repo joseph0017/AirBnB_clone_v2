a558470d07755d12787017379cf5127779ec6120
joseph0017 <josephogiku96@gmail.com>
2023-04-21 04:34:48 -0700

    flask

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -42,7 +42,7 @@ def display_number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def display_number_template(n):
     """sends a GET request to the number_template page"""
-    return render_template('5-number.html')
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
