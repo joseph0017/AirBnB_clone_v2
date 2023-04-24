8945ee6e0835889fc55de1c54c56377bc5e8eb57
joseph0017 <josephogiku96@gmail.com>
2023-04-24 11:06:45 -0700

    not what i want

--- console.py
+++ console.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+
 """ Console Module """
 import cmd
 import sys
@@ -114,27 +114,16 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        try:
-            if not args:
-                raise SyntaxError()
-            split1 = args.split(' ')
-            new_instance = eval('{}()'.format(split1[0]))
-            params = split1[1:]
-            for param in params:
-                k, v = param.split('=')
-                try:
-                    attribute = HBNBCommand.verify_attribute(v)
-                except:
-                    continue
-                if not attribute:
-                    continue
-                setattr(new_instance, k, attribute)
-            new_instance.save()
-            print(new_instance.id)
-        except SyntaxError:
+        if not args:
             print("** class name missing **")
-        except NameError as e:
+            return
+        elif args not in HBNBCommand.classes:
             print("** class doesn't exist **")
+            return
+        new_instance = HBNBCommand.classes[args]()
+        storage.save()
+        print(new_instance.id)
+        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
