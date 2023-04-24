3d935ddee9d117a8540ac69b15a49e6c291ef5c5
joseph0017 <joseephogiku96@.com>
2023-04-24 15:46:15 -0700

    all about that fabric

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
--- Creates the database hbnb_dev_db 
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
-SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
-GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-=======
 -- Creates the database hbnb_dev_db with specified parameters
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
@@ -14,4 +5,3 @@ ALTER USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
->>>>>>> 198e92b61384971e960e98900967f4f0ac720045
